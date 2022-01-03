fe37ff7959dd927252fc7995f44d05777edf4856
freddocodes <96920050+freddocodes@users.noreply.github.com>
2022-01-03 23:00:44 +0000

    this version should work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 st.title('Our First Streamlit App')
-st.sub('Introduction to Streamlit in Class')
-st.write('this is my first Web App.
-Enjoy it !
-''')
+st.subheader('Introduction to Streamlit in Class')
+st.write('this is my first Web App.')
