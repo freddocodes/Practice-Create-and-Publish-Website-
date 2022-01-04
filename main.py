f66d23fece529b656cfdb3c9b4b1f1f747c2b9ff
freddocodes <96920050+freddocodes@users.noreply.github.com>
2022-01-04 00:43:41 +0000

    added pandas data series

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas
+
+
+data= {
+  'Series_1':[1, 3, 4, 5, 7],
+  'Series_2':[10, 30, 40, 100, 250]
+}
+
+df= pandas.DataFrame(data)
 
 st.title('Our First Streamlit App')
 st.subheader('Introduction to Streamlit in Class')
 st.write('this is my first Web App.')
+st.write(df)
